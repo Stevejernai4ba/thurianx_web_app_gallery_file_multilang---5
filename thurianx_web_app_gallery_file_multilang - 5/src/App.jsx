ae1caead9cb2fd9117aff911b156b483d34aefab
ThurianX <138269799+Stevejernai4ba@users.noreply.github.com>
2025-05-25 01:06:58 +0700

    Update App.jsx

--- thurianx_web_app_gallery_file_multilang - 5/src/App.jsx
+++ thurianx_web_app_gallery_file_multilang - 5/src/App.jsx
@@ -8,14 +8,14 @@ function WelcomeScreen({ onStart, lang, setLang }) {
   useEffect(() => {
     const playAudioOnce = () => {
       const audio = new Audio('/Sia.mp3.mp3');
-      audio.currentTime = 9;
+      audio.currentTime = 0;
       audio.volume = 0.2;
       audio.play().catch(() => {});
       window.__thurianxAudio = audio;
 
       // Stop at 37 seconds
       const stopAt = setInterval(() => {
-        if (audio.currentTime >= 21) {
+        if (audio.currentTime >= 37) {
           audio.pause();
           clearInterval(stopAt);
         }
